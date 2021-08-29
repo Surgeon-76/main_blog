a6e607981749c35e010c00cb3da22e5a1887e40c
Surgeon76 <Surgeon76@yandex.ru>
2021-08-29 11:21:23 +0300

    1

--- config/db_config.py
+++ config/db_config.py
@@ -1,10 +1,14 @@
-from sqlalchemy import MetaData, create_engine
-from databases import *
+import databases
+import sqlalchemy
+#import sqlalchemy
+#from sqlalchemy import MetaData, engine
+#from databases import *
+#import sqlalchemy
 
 user_name_db = 'surglin'
 password_db = 'Nusha230399'
 db_name = 'test_db'
 
 database = databases.Database(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
-metadata = MetaData()
-engine = create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
+metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
