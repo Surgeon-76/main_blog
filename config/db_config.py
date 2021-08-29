ace1ad846798638341535383885c718c2c000e4c
Surgeon76 <Surgeon76@yandex.ru>
2021-08-29 04:18:07 +0300

    config db

--- config/db_config.py
+++ config/db_config.py
@@ -1,1 +1,11 @@
-import databases
+from sqlalchemy import engine
+import databases,sqlalchemy
+
+
+user_name_db = 'surglin'
+password_db = 'Nusha230399'
+db_name = 'test_db'
+
+database = databases.Database(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
+metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
