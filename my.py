4f7487541d5a8cc1d84261efa13747922e064e01
Surgeon76 <Surgeon76@yandex.ru>
2021-08-31 23:58:12 +0300

    Заработало!!!!! УУУрррааа!!!!

--- my.py
+++ my.py
@@ -5,7 +5,7 @@ from config.db_config import engine, metadata, database
 from models.ballons import *
 from models.cars import *
 from models.users import *
-
+from routes import users, cars, ballons
 
 
 app = FastAPI()
@@ -23,4 +23,20 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+app.include_router(
+    users.user_route,
+    prefix='/users',
+    tags=['Корытодержатели'],
+)
 
+app.include_router(
+    cars.cars_route,
+    prefix='/cars',
+    tags=['Сами КорытА'],
+)
+
+app.include_router(
+    ballons.ballons_route,
+    prefix='/ballons',
+    tags=['Тапки'],
+)
